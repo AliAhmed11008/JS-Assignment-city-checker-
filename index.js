fb66fb9dd6c6edb97cee78cb9d15e7acb168f683
AliAhmed11008 <aliahmed110087@gmail.com>
2025-01-15 18:53:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const cities = [
+    "Karachi", "Lahore", "Islamabad", "Peshawar", "sialkot", "Multan", "Rawalpindi", "Okara", "Sahiwal", "Hyderabad", "Sukkur", "Quetta", "Larkana", "Dera Gazi Khan", "Mirpur Khas", "Bahawalpur", "Chiniot"
+
+];
+
+if (cities.includes("Islamabad")) {
+    alert("Islamabad is the cleanest city of Pakistan");
+}
